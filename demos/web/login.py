842b8d0b822e2ed6f4c1723dba2a1ad19bda7425
wmbrown18 <wmbrown18@gmail.com>
2017-07-20 08:59:22 -0400

    Add files via upload

---
+++ demos/web/login.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import MySQLdb as mdb
+import sys
+
+con=None
+
+try:
+    con = mdb.connect('localhost', 'wbrown220', 'inaveman', 'employees');
+
+    cur = con.cursor()
+    cur.execute("SELECT VERSION()")
+
+    ver = cur.fetchone()
+    
+    print "Database version : %s " % ver
+    
+except mdb.Error, e:
+  
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    sys.exit(1)
+    
+finally:    
+        
+    if con:    
+        con.close()
